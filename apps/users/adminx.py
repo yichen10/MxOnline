3d9f29b68f4a87a8b449441614daa368e37d1926
wangyaojin <wangyaojin15@gmail.com>
2017-09-08 18:04:45 +0800

    增加全局变量

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -2,8 +2,18 @@
 
 
 import xadmin
-
+from xadmin import views
 from .models import EmailVerifyRecord, Banner
+
+
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+
+
+class GlobalSettings(object):
+    site_title = "暮雪网后台管理系统"
+    site_footer = "暮雪在线网"
 
 
 class EmailVerifyRecordAdmin(object):
@@ -13,10 +23,12 @@ class EmailVerifyRecordAdmin(object):
 
 
 class BannerAdmin(object):
-    list_display = ['code', 'email', 'send_type', 'send_time']
-    search_fields = ['code', 'email', 'send_type']
-    list_filter = ['code', 'email', 'send_type']
+    list_display = ['title', 'image', 'url', 'index', 'add_time']  # 后台自定义显示列 显示字段
+    search_fields = ['title', 'image', 'url', 'index']  # 定义后台搜索 搜索功能
+    list_filter = ['title', 'image', 'url', 'index', 'add_time']  # 过滤器 通过时间搜索
 
 
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
 xadmin.site.register(Banner, BannerAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSettings)
